82d7605935752612301486cdad72f12eacb09334
msvr <saivivek512@gmail.com>
2016-02-24 01:20:15 +0530

    fragment and button

--- Online2/app/src/main/java/com/example/android/online2/MainActivity.java
+++ Online2/app/src/main/java/com/example/android/online2/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.android.online2;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
@@ -7,18 +10,79 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     DrawerLayout mDrawerLayout;
     NavigationView mNavigationView;
     FragmentManager mFragmentManager;
     FragmentTransaction mFragmentTransaction;
+    Menu m ;
+    String url ;
+    SharedPreferences sharedPreferences;
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    RequestQueue rq ;
+    MainActivity instance1;
+    Button butt;
+
+
+    //variables for courses storage
+    String[] c_array;
+    String current_sem ;
+    String current_year ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        instance1 = this ;
+
+        rq = Volley.newRequestQueue(this);
+
+
+        sharedPreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+        url="http://192.168.23.1:8000/courses/list.json";
+        StringRequest stringRequest=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                try {
+                    JSONObject jsonObject= new JSONObject(s);
+
+
+                    if(jsonObject.getBoolean("success")){
+
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+
+            }
+        }, this);
+        rq.add(stringRequest);
+
+
 
         /**
          *Setup the DrawerLayout and NavigationView
@@ -27,11 +91,19 @@ public class MainActivity extends AppCompatActivity{
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         mNavigationView = (NavigationView) findViewById(R.id.shitstuff) ;
 
+        m = mNavigationView.getMenu();
+        m.add(1,5,0,"Foo5");
+        m.add(1,6,0,"Foo6");
+        m.add(1,7,0,"Foo7");
+
+
         /**
          * Lets inflate the very first fragment
          * Here , we are inflating the TabFragment as the first Fragment
          */
-
+        mFragmentManager = getSupportFragmentManager();
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+        //mFragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
         /**
          * Setup click events on the Navigation View Items.
          */
@@ -43,15 +115,20 @@ public class MainActivity extends AppCompatActivity{
 
 
 
-                if (menuItem.getItemId() == R.id.nav_item_sent) {
+                if (menuItem.getItemId() == 5) {
                     FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
+                    fragmentTransaction.replace(R.id.containerView,new PrimaryFragment()).commit();
+                }
+                if (menuItem.getItemId() == 6) {
+                    FragmentTransaction yfragmentTransaction = mFragmentManager.beginTransaction();
+                    yfragmentTransaction.replace(R.id.containerView,new SocialFragment()).commit();
 
                 }
 
                 if (menuItem.getItemId() == R.id.nav_item_inbox) {
                     FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
-                    xfragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
+                    xfragmentTransaction.replace(R.id.containerView,new PrimaryFragment()).commit();
+
                 }
 
                 return false;
@@ -71,5 +148,11 @@ public class MainActivity extends AppCompatActivity{
 
         mDrawerToggle.syncState();
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+        xfragmentTransaction.replace(R.id.containerView,new SocialFragment()).commit();
     }
 }
--- Online2/app/src/main/java/com/example/android/online2/PrimaryFragment.java
+++ Online2/app/src/main/java/com/example/android/online2/PrimaryFragment.java
@@ -2,19 +2,46 @@ package com.example.android.online2;
 
 
 
+        import android.content.Intent;
         import android.os.Bundle;
         import android.support.annotation.Nullable;
         import android.support.v4.app.Fragment;
+        import android.support.v4.app.FragmentManager;
+        import android.support.v4.app.FragmentTransaction;
         import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
+        import android.widget.Button;
+        import android.widget.Toast;
 
 
-public class PrimaryFragment extends Fragment {
+public class PrimaryFragment extends Fragment{
+
+
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.primary_layout,null);
+        View v = inflater.inflate(R.layout.primary_layout,null);
+        Button butt = (Button) v.findViewById(R.id.butt);
+        //Put the value
+        butt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SocialFragment ldf = new SocialFragment();
+                Bundle args = new Bundle();
+                args.putString("YourKey", "YourValue");
+                ldf.setArguments(args);
+
+            //Inflate the fragment
+                getFragmentManager().beginTransaction().replace(R.id.containerView, new SocialFragment()).commit();
+            }
+        });
+
+
+        return v;
+
     }
+
 }
